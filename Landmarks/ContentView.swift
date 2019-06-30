26bd5c8e46c03e88d9864aa8ef99a7a8f099f3a1
Jeff Small <jeffreyd.small@gmail.com>
2019-06-30 16:19:01 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jeff on 6/30/19.
+//  Copyright © 2019 Jeff Small. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
