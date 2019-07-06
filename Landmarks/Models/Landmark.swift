9570d0782a50070ea1e5434e795332e863df6b93
Jeff Small <jeffreyd.small@gmail.com>
2019-07-06 15:11:53 -0700

    Updated the Landmark model
    Added an isFavorite property and a `mountains` case to the Category enum

--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -10,7 +10,7 @@
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     fileprivate var imageName: String
@@ -18,6 +18,7 @@ struct Landmark: Hashable, Codable {
     var state: String
     var park: String
     var category: Category
+    var isFavorite: Bool
 
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
@@ -33,6 +34,7 @@ struct Landmark: Hashable, Codable {
         case featured = "Featured"
         case lakes = "Lakes"
         case rivers = "Rivers"
+        case mountains = "Mountains"
     }
 }
 
